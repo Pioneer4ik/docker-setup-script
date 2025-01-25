ee8d4f94c6d9a3e74ccd7ea0248dcd4472cc6b6c
Pioneer4ik <164672631+Pioneer4ik@users.noreply.github.com>
2025-01-25 02:20:30 +0200

    Update titan.sh

--- titan.sh
+++ titan.sh
@@ -8,6 +8,7 @@ install_docker() {
   echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
   sudo apt update && sudo apt install -y docker-ce docker-ce-cli containerd.io
   sudo usermod -aG docker $USER
+  sudo sh get-docker.sh
   echo "Docker установлен и настроен."
 }
 
